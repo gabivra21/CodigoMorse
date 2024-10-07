b20d7f691cb17a5c10c07847fa98362716de56bf
Gabriela Vieira Ramos <gabrielavieirar21@gmail.com>
2024-10-07 20:10:58 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,6 @@
-
-
 public class Main {
     public static void main(String[] args) {
-        Arvore arvore = new Arvore();
-
-        arvore.generateMorseTreeFromUserInput();
-
+        new ArvoreGUI();
 
     }
 }
